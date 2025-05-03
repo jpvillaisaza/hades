8711be6b36976b120d03656e3f0c74a9e91a58cf
Juan Pedro Villa Isaza <584947+jpvillaisaza@users.noreply.github.com>
2025-05-02 19:27:51 -0500

    Add hangman

---
+++ src/Hades/Game.hs
@@ -0,0 +1,4 @@
+module Hades.Game where
+
+data Game
+  = Hangman
--- src/Hades/Hangman.hs
+++ src/Hades/Hangman.hs
@@ -1,5 +1,36 @@
 module Hades.Hangman (hangman) where
 
+import Control.Monad (unless)
+
 hangman :: IO ()
-hangman =
+hangman = do
   putStrLn "Hangman"
+  let word = "hangman"
+  play 1 word (fmap (, False) word) mempty
+
+play :: Int -> String -> [(Char, Bool)] -> [Char] -> IO ()
+play n word guessed previous = do
+  putStrLn (fmap (\(x, b) -> if b then x else '-') guessed)
+  unless (null previous) (putStrLn ("Previous guesses: " <> previous))
+  putStr ("Guess " <> show n <> "? ")
+  guess <- getLine
+  case guess of
+    [] ->
+      play n word guessed previous
+    [guess1] -> do
+      let new = update guess1 guessed
+      if and (fmap snd new)
+        then
+          putStrLn "You got it!"
+        else
+          play (succ n) word new (guess1:previous)
+    _ ->
+      if guess == word
+        then
+          putStrLn "You got it!"
+        else
+          play (succ n) word guessed previous
+
+update :: Char -> [(Char, Bool)] -> [(Char, Bool)]
+update guess guessed =
+  [(letter, isGuessed || guess == letter) | (letter, isGuessed) <- guessed]
